06ad4dababe5e7136cb19a532e8e93956f753cf7
Kyle Zhou <kylezhou2002@hotmail.com>
2015-04-18 00:30:05 -0400

    Added basic CPU controller

---
+++ SticksAndPhones/Assets/Standard Assets/2D/Scripts/Platformer2DCPUControl.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DCPUControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                //m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Jump = false;
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = false; // Input.GetKey(KeyCode.LeftControl);
+            float h = -0.5f; //CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
