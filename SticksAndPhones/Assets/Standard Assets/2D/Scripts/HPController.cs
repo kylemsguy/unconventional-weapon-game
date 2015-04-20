adeec21f0292d02cff42ac3df72a3f88210b9782
Tbarpo <taimur.rashid1@gmail.com>
2015-04-19 21:44:27 -0400

    Implemented hp system.

---
+++ SticksAndPhones/Assets/Standard Assets/2D/Scripts/HPController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+    
+public class HPController : MonoBehaviour {
+
+    [SerializeField] private PlatformerCharacter2D player1;
+    [SerializeField] private PlatformerCharacter2D player2;
+
+	// Use this for initializationS
+	void Awake() {
+        player1 = (PlatformerCharacter2D)GameObject.Find("Player1").GetComponent<PlatformerCharacter2D>();
+        player2 = (PlatformerCharacter2D)GameObject.Find("Player2").GetComponent<PlatformerCharacter2D>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        transform.Find("HP_P1").Find("HPBar").transform.localScale = new Vector3(player1.GetHealth() / 100, 1, 1);
+        transform.Find("HP_P2").Find("HPBar").transform.localScale = new Vector3(player2.GetHealth() / 100, 1, 1);
+	}
+}
